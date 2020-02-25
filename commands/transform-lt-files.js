4400ac6aacc1aaabb543b127d8cdf8204d66eeca
Elijah <ejsisgreat@gmail.com>
2020-02-24 18:22:28 -0600

    Require on the go

--- commands/transform-lt-files.js
+++ commands/transform-lt-files.js
@@ -1,10 +1,9 @@
-import { compile } from '../compiler/index';
-
 export default {
 	name: `transform-files`,
-	transform: (oldCode, id) => {
+	transform: async (oldCode, id) => {
 		if (/globfile\?.+/.test(id)) return null;
 
+		const { compile } = require('../compiler/index');
 		const { code, sourcemap } = compile(oldCode);
 
 		return code;
