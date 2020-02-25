5c8f35443fd3c0fdfb9e26eb823c2e6a10d03e4b
Elijah <ejsisgreat@gmail.com>
2020-02-25 08:56:28 -0600

    Require from the config

--- commands/transform-lt-files.js
+++ commands/transform-lt-files.js
@@ -3,7 +3,7 @@ export default {
 	transform: async (oldCode, id) => {
 		if (/globfile\?.+/.test(id)) return null;
 
-		const { compile } = require('../compiler/index');
+		const { compile } = require('./compiler/index');
 		const { code, sourcemap } = compile(oldCode);
 
 		return code;
