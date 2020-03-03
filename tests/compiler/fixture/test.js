82701714cc41fb4eefe3f238f4c98eee83b5e97e
Elijah Mooring <ejsisgreat@gmail.com>
2020-03-03 13:47:13 -0600

    Add some more comments to the live tests

--- tests/compiler/fixture/test.js
+++ tests/compiler/fixture/test.js
@@ -1,11 +1,18 @@
-console.log(`\n\n\nsayHello should be called twice.  Once for Jerry and once for Bob.`);
+console.log(
+	`\n\n\nsayHello should be called three times.  Once for Jerry, once for Bob, and once when 'sayHello' itself changes on line 14.`
+);
 
-function sayHello(name) {
+let sayHello = name => {
 	console.log(`Hello, ${name}`);
-}
+};
 
 let name = `Jerry`;
 
+// This means, "Run `sayHello(name)` everytime the value of `sayHello` or `name` changes".
 $: sayHello(name);
 
 name = `Bob`;
+
+sayHello = name => {
+	console.log(`Hi, ${name} - sayHello just changed`);
+};
