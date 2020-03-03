6d96cf108fc1eba3e5fc1ca1c8b482fc8e06fd61
Elijah Mooring <ejsisgreat@gmail.com>
2020-03-03 13:31:13 -0600

    Add some comments for clarification

--- tests/compiler/fixture/many-scopes.js
+++ tests/compiler/fixture/many-scopes.js
@@ -3,11 +3,14 @@ console.log(`\n\n\nExpected output:  sayHello will be called once, but the 'cons
 function sayHello(name) {
 	console.log(`Called once`);
 
+	// Because a reactive `name` was passed into this function on line 14,
+	// we can call the `console.log` every time it changes.
 	$: console.log(`Hello, ${name} - called twice`);
 }
 
 let name = `Jerry`;
 
+// The `.valueOf()` tells the compiler to "proxy" the "reactiveness" of `name` on to `sayHello`
 sayHello(name.valueOf());
 
 name = `Bob`;
